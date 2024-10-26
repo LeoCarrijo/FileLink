be5f595bdfdd95b30d5e434fc2fe07d079a6474c
LeoCarrijo <lh83712@gmail.com>
2024-10-26 18:25:05 -0300

    :sparkles: feat: Adicionando logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,11 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Image from "next/image"
+import logo from "@/public/logo.png"
 
 export default function Header() {
     return (
         <header>
-            <img src="" alt="Logo" />
+            <Image src={logo} width={100} height={100} alt="logo"/>
             <h1>FileLink</h1>
             <Avatar>
                 <AvatarImage src="https://github.com/shadcn.png" />

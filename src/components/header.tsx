284aa49dc0f09a422540dc6a593f9ab3e9b1d305
LeoCarrijo <lh83712@gmail.com>
2024-10-26 18:57:22 -0300

    :sparkles: feat: Implementação do Biome

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,16 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import Image from "next/image"
-import logo from "@/public/logo.png"
+import Image from "next/image";
+import logo from "@/public/logo.png";
 
 export default function Header() {
-    return (
-        <header>
-            <Image src={logo} width={100} height={100} alt="logo"/>
-            <h1>FileLink</h1>
-            <Avatar>
-                <AvatarImage src="https://github.com/shadcn.png" />
-                <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-        </header>
-    )
+	return (
+		<header className="p-10 justify-between bg-gradient-to-r from-sky-500 to-indigo-50 flex">
+			<Image src={logo} width={100} height={100} alt="logo" />
+			<h1 className="font-bold text-3xl underline">FileLink</h1>
+			<Avatar>
+				<AvatarImage src="https://github.com/shadcn.png" />
+				<AvatarFallback>CN</AvatarFallback>
+			</Avatar>
+		</header>
+	);
 }

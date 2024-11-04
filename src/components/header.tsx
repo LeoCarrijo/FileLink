c7d27adc4e4d359fef9cc3d4c53fc6f916093b70
LeoCarrijo <lh83712@gmail.com>
2024-11-04 19:42:57 -0300

    :lipstick: style: Refazendo os estilos do cabeçalho

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,9 +4,11 @@ import logo from "@/public/logo.png";
 
 export default function Header() {
 	return (
-		<header className="flex justify-between bg-gradient-to-r from-sky-500 to-indigo-50 p-10">
+		<header className="flex justify-between p-10">
 			<Image src={logo} width={100} height={100} alt="logo" />
-			<h1 className="font-bold text-3xl underline">FileLink</h1>
+			<h1 className="lobster-regular font-bold text-5xl text-white underline">
+				FileLink
+			</h1>
 			<Avatar>
 				<AvatarImage src="https://github.com/shadcn.png" />
 				<AvatarFallback>CN</AvatarFallback>

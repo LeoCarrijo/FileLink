2fecf0d879d99ac3ab45378cc11af54cd360c7a8
LeoCarrijo <lh83712@gmail.com>
2024-11-11 20:32:57 -0300

    🧹 cleanup: Removendo importações desnecessárias

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,6 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import Main from "@/components/main";
-import { ModeToggle } from "@/components/theme-toggle";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { useState } from "react";
 
 export default function Home() {
 	return (
